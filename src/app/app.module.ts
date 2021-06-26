36978c0c578e0545dface3a07ebe3a4245ed531b
thunderawesome <troyjsanders@gmail.com>
2021-06-25 22:37:36 -0500

    Removed youtube player angular package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { HelloComponent } from './hello.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, YouTubePlayerModule],
+  imports: [BrowserModule, FormsModule, ],
   declarations: [AppComponent, WelcomeComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
